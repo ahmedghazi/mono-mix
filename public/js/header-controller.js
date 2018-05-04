738476cbf8fc29b1a7b0827d86a4dba58c6e193c
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2018-05-04 11:51:05 +0200

    notes

---
+++ public/js/header-controller.js
@@ -0,0 +1,20 @@
+var headerController = (function () {
+    
+    init()
+    
+    function init() {
+        bind_events()
+    }
+
+    function bind_events() {
+        $("html").on("mouseenter", "h1", function(){
+            var rand = 1 + Math.round(Math.random() * 2);
+            console.log(rand)
+            $(this).attr("data-idx", rand)
+        });
+
+        
+    
+    }
+
+})();
